48d02355084d2c8f3837c6dde9644ba3f70ad4a9
ABDALLAH-XX <abdallahhafidhou@hotmail.com>
2026-04-16 21:20:09 +0200

    Image grayscale + instructions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # FASTAPI Tutorial
 
-
+# Mon API de traitement d'images
+Pour lancer l'API : `uvicorn main:app --reload`
+Pour tester l'API : aller sur `http://127.0.0.1:8000/docs`
--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse # <--- Pour renvoyer l'image
 from PIL import Image
 import io
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenue sur l'API de traitement d'image !"}
-
-@app.post("/image-info/")
-async def get_image_info(file: UploadFile = File(...)):
-    # 1. Lire le contenu du fichier uploadé
-    request_object_content = await file.read()
+@app.post("/image-grayscale/")
+async def make_grayscale(file: UploadFile = File(...)):
+    # 1. Lire l'image uploadée
+    content = await file.read()
+    img = Image.open(io.BytesIO(content))
     
-    # 2. Ouvrir l'image avec Pillow
-    img = Image.open(io.BytesIO(request_object_content))
+    # 2. Traitement : Convertir en Noir et Blanc (mode 'L')
+    img_gray = img.convert("L")
     
-    # 3. Extraire les infos
-    return {
-        "filename": file.filename,
-        "format": img.format,
-        "mode": img.mode,
-        "width": img.width,
-        "height": img.height
-    }
+    # 3. Préparer le flux de sortie pour renvoyer l'image
+    img_buffer = io.BytesIO()
+    # On sauvegarde dans le buffer au format PNG par exemple
+    img_gray.save(img_buffer, format="PNG")
+    img_buffer.seek(0) # On remet le curseur au début pour la lecture
+    
+    return StreamingResponse(img_buffer, media_type="image/png")
